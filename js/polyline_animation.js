3f24f4d92e42801e8a8c22f77f7f13a76794b55a
Laptev Anton <antonlaptew@gmail.com>
2019-12-29 16:01:51 +0600

    use promises; load json gps dataset from file;

--- js/polyline_animation.js
+++ js/polyline_animation.js
@@ -1,47 +1,48 @@
 "use strict";
 
-ymaps.ready(['AnimatedLine']).then(init);
+const DEBUG = true
+if (DEBUG !== true) { console.log = ()=>{} }
 
-function init(ymaps) {
-  const DEBUG = true
-  if (DEBUG !== true) { console.log = ()=>{} }
+// 1. load json data; resolve(data)
+// 2. (data) -> filter the data; resolve(filtered_data)
+// 3. (filtered_data) -> init map
 
-  console.log('---log---  init')
+$.getJSON("gps_kropotkino.json")
+  .then( (dataSet) => {
+    console.log('---log--- data = ', dataSet);
+    console.log('---log--- dataSet.length = ', dataSet.length)
 
-  const dataSet = [
-    [45.345374032,40.138917891,"2019-11-02 06:44:08.199999"],
-    [45.345373894,40.138917596,"2019-11-02 06:44:08.400000"],
-    [45.345373764,40.138917889,"2019-11-02 06:44:08.600000"],
-    [45.345374022,40.138917860,"2019-11-02 06:44:08.800000"],
-    [45.345373433,40.138917791,"2019-11-02 06:44:09.000000"],
-    [45.345373717,40.138918135,"2019-11-02 06:44:09.200000"],
-    [45.345372592,40.138918415,"2019-11-02 06:44:09.400000"],
-    [45.345372264,40.138918689,"2019-11-02 06:44:09.600000"],
-    [45.345372361,40.138919232,"2019-11-02 06:44:09.800000"],
-    [45.345372189,40.138920146,"2019-11-02 06:44:10.000000"]
-  ]
-  console.log('---log--- dataSet.length = ', dataSet.length)
-
-  function filterDataSet(coords, minDistance) {
-    let newCoords = [];
-    newCoords.push(coords[0]);
-    for (let i = 1; i < coords.length; i++) {
-      let distance = getDistance(newCoords[newCoords.length-1], coords[i]);
-      if ((distance >= minDistance) || (i === coords.length-1)) {
-        newCoords.push(coords[i])
+    function filterDataSet(coords, minDistance) {
+      let newCoords = [];
+      newCoords.push(coords[0]);
+      for (let i = 1; i < coords.length; i++) {
+        let distance = getDistance(newCoords[newCoords.length-1], coords[i]);
+        if ((distance >= minDistance) || (i === coords.length-1)) {
+          newCoords.push(coords[i])
+        }
       }
+      return newCoords;
     }
-    return newCoords;
-  }
 
-  // Функция нахождения расстояния между двумя точками на плоскости.
-  function getDistance(point1, point2) {
-    const d = Math.sqrt( Math.pow((point2[0] - point1[0]), 2) + Math.pow((point2[1] - point1[1]), 2) );
-    // console.log('---log--- distance = ', d)
-    return d
-  }
+    function getDistance(point1, point2) {
+      return Math.sqrt( Math.pow((point2[0] - point1[0]), 2) + Math.pow((point2[1] - point1[1]), 2) );
+    }
 
-  const filteredDataSet = filterDataSet(dataSet, 0.000003)
+    const filteredDataSet = filterDataSet(dataSet, 0.000003)
+    console.log('---log--- filteredDataSet.length = ', filteredDataSet.length)
+
+    return filteredDataSet;
+  }).then( (filteredDataSet) => {
+    console.log('---log--- filteredDataSet.length = ', filteredDataSet.length)
+    ymaps.ready(['AnimatedLine']).then((ymaps) => {
+      console.log('---log--- ymaps = ', ymaps);
+      init(ymaps, filteredDataSet);
+    })
+
+  });
+
+function init(ymaps, filteredDataSet) {
+  console.log('---log--- init')
   console.log('---log--- filteredDataSet.length = ', filteredDataSet.length)
 
   // Создаем карту.
@@ -90,13 +91,3 @@ function init(ymaps) {
   // Запускаем анимацию пути.
   playAnimation();
 }
-
-// let str = 'string'
-// const jsonStr = JSON.stringify(str)
-// str = JSON.parse(jsonStr)
-// console.log('---log--- str = ', str)
-
-// jQuery.getJSON('data.json', function (json) {
-//   console.log('---log--- json = ', json)
-// });
-
